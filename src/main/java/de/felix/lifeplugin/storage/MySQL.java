bbc2352d72e772986d7d4ee1f2ebcc10472ddcb3
Felixwrn <104013013+Felixwrn@users.noreply.github.com>
2026-04-25 23:37:36 +0200

    Refactor MySQL class for better structure and functionality

--- src/main/java/de/felix/lifeplugin/storage/MySQL.java
+++ src/main/java/de/felix/lifeplugin/storage/MySQL.java
@@ -1,22 +1,68 @@
-package de.felix.lifeplugin;
+package de.felix.lifeplugin.storage;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.UUID;
 
-public class MySQL {
+public class MySQL implements Storage {
 
-    public Connection connect(String host, String database, String user, String password) {
+    private Connection connection;
 
+    public MySQL(String host, int port, String db, String user, String pass) {
         try {
-            String url = "jdbc:mysql://" + host + ":3306/" + database;
+            connection = DriverManager.getConnection(
+                    "jdbc:mysql://" + host + ":" + port + "/" + db,
+                    user,
+                    pass
+            );
 
-            return DriverManager.getConnection(url, user, password);
+            PreparedStatement ps = connection.prepareStatement(
+                    "CREATE TABLE IF NOT EXISTS lives (uuid VARCHAR(36) PRIMARY KEY, lives INT)"
+            );
+            ps.execute();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public int getLives(UUID uuid) {
+        try {
+            PreparedStatement ps = connection.prepareStatement(
+                    "SELECT lives FROM lives WHERE uuid=?"
+            );
+            ps.setString(1, uuid.toString());
+
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                return rs.getInt("lives");
+            }
 
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
-        return null;
+        return 10;
+    }
+
+    @Override
+    public void setLives(UUID uuid, int lives) {
+        try {
+            PreparedStatement ps = connection.prepareStatement(
+                    "REPLACE INTO lives (uuid, lives) VALUES (?, ?)"
+            );
+            ps.setString(1, uuid.toString());
+            ps.setInt(2, lives);
+            ps.execute();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void save(UUID uuid) {
+        // nichts nötig
     }
 }
