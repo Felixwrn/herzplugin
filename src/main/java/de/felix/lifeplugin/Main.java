242c465eec91f99e67dbf062064d2e9ec68b9c6d
Felixwrn <104013013+Felixwrn@users.noreply.github.com>
2026-04-27 16:59:09 +0200

    Refactor Main class and remove unused code

--- src/main/java/de/felix/lifeplugin/Main.java
+++ src/main/java/de/felix/lifeplugin/Main.java
@@ -1,25 +1,16 @@
 package de.felix.lifeplugin;
 
-import de.felix.lifeplugin.gui.LanguageGUI;
-import de.felix.lifeplugin.gui.LifeGUI;
-import de.felix.lifeplugin.gui.ModeBuilderGUI;
-import de.felix.lifeplugin.gui.ModeGUI;
-import de.felix.lifeplugin.gui.MarketplaceGUI;
-import de.felix.lifeplugin.lang.LanguageManager;
-import de.felix.lifeplugin.storage.*;
+import de.felix.lifeplugin.gui.*;
+import de.felix.lifeplugin.market.MarketplaceManager;
 import de.felix.lifeplugin.util.ChatInput;
+import com.google.gson.JsonObject;
 
-import org.bukkit.Bukkit;
-import org.bukkit.Material;
-import org.bukkit.Sound;
+import org.bukkit.*;
 import org.bukkit.command.*;
 import org.bukkit.entity.Player;
 import org.bukkit.event.*;
-import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.inventory.*;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.configuration.file.YamlConfiguration;
 
 import java.io.*;
 import java.net.URL;
@@ -27,159 +18,37 @@ import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
 import java.util.*;
 
-public class Main extends JavaPlugin implements Listener, TabExecutor {
+public class Main extends JavaPlugin implements Listener {
 
     private static Main instance;
 
-    private LanguageManager languageManager;
-    private Storage storage;
-
-    private final HashMap<UUID, Integer> lives = new HashMap<>();
-
-    private String mode;
-
-    private File langConfigFile;
-    private YamlConfiguration langConfig;
+    public static Main getInstance() {
+        return instance;
+    }
 
     @Override
     public void onEnable() {
 
         instance = this;
 
-        saveDefaultConfig();
-        loadLangConfig();
-        copyDefaultLanguages();
-
-        languageManager = new LanguageManager();
-        languageManager.load(new File(getDataFolder(), "lang"));
-
-        mode = getConfig().getString("mode", "LIFESTEAL");
-
-        String type = getConfig().getString("storage.type");
-
-        if ("MYSQL".equalsIgnoreCase(type)) {
-            storage = new MySQLStorage(
-                    getConfig().getString("mysql.host"),
-                    getConfig().getInt("mysql.port"),
-                    getConfig().getString("mysql.database"),
-                    getConfig().getString("mysql.user"),
-                    getConfig().getString("mysql.password")
-            );
-        } else {
-            storage = new FileStorage(getDataFolder());
-        }
-
         getServer().getPluginManager().registerEvents(this, this);
         getServer().getPluginManager().registerEvents(new ChatInput(), this);
 
-        getCommand("mode").setExecutor(this);
-        getCommand("mode").setTabCompleter(this);
-
-        // 🔥 Daily Marketplace Update
+        // Daily Marketplace Reload
         Bukkit.getScheduler().runTaskTimer(this, () -> {
             MarketplaceGUI.reload();
         }, getTicksUntilMidnight(), 20L * 60 * 60 * 24);
-
-        getLogger().info("LifePlugin enabled!");
     }
 
-    // ---------------- LANGUAGE ----------------
-
-    private void loadLangConfig() {
-        langConfigFile = new File(getDataFolder(), "languages.yml");
-
-        if (!langConfigFile.exists()) {
-            saveResource("languages.yml", false);
-        }
-
-        langConfig = YamlConfiguration.loadConfiguration(langConfigFile);
-    }
-
-    private void copyDefaultLanguages() {
-        File langFolder = new File(getDataFolder(), "lang");
-
-        if (!langFolder.exists()) langFolder.mkdirs();
-
-        String[] defaults = {"de.json", "en.json"};
-
-        for (String file : defaults) {
-            File target = new File(langFolder, file);
-            if (!target.exists()) {
-                saveResource("lang/" + file, false);
-            }
-        }
-    }
-
-    public static Main getInstance() {
-        return instance;
-    }
-
-    public LanguageManager getLanguageManager() {
-        return languageManager;
-    }
-
-    public YamlConfiguration getLangConfig() {
-        return langConfig;
-    }
-
-    public int getLives(UUID uuid) {
-        return lives.getOrDefault(uuid, getConfig().getInt("start-lives", 10));
-    }
-
-    // ---------------- EVENTS ----------------
-
-    @EventHandler
-    public void onJoin(PlayerJoinEvent e) {
-        Player p = e.getPlayer();
-
-        int loaded = storage.getLives(p.getUniqueId());
-        lives.put(p.getUniqueId(), loaded);
-
-        updateActionBar(p);
-    }
-
-    @EventHandler
-    public void onDeath(PlayerDeathEvent e) {
-
-        Player p = e.getEntity();
-
-        int current = getLives(p.getUniqueId()) - 1;
-
-        lives.put(p.getUniqueId(), current);
-        storage.setLives(p.getUniqueId(), current);
-        storage.save(p.getUniqueId());
-
-        if (current <= 0) {
-            p.kickPlayer(languageManager.get(p.getUniqueId(), "no_lives"));
-            return;
-        }
-
-        Player killer = p.getKiller();
-
-        if (killer != null && mode.equalsIgnoreCase("LIFESTEAL")) {
-
-            int steal = getConfig().getInt("lifesteal.steal-amount", 1);
-            int max = getConfig().getInt("lifesteal.max-lives", 20);
-
-            int newLives = Math.min(getLives(killer.getUniqueId()) + steal, max);
-
-            lives.put(killer.getUniqueId(), newLives);
-            storage.setLives(killer.getUniqueId(), newLives);
-            storage.save(killer.getUniqueId());
-        }
-    }
-
-    private void updateActionBar(Player p) {
-        String msg = languageManager.format(
-                p.getUniqueId(),
-                "lives",
-                "lives", String.valueOf(getLives(p.getUniqueId()))
+    // ---------------- ACTION BAR FIX ----------------
+    public void sendActionBar(Player p, String msg) {
+        p.spigot().sendMessage(
+                net.md_5.bungee.api.ChatMessageType.ACTION_BAR,
+                new net.md_5.bungee.api.chat.TextComponent(msg)
         );
-        p.sendActionBar(msg);
     }
 
-    // ---------------- GUI ----------------
-
+    // ---------------- GUI EVENTS ----------------
     @EventHandler
     public void onClick(InventoryClickEvent e) {
 
@@ -201,81 +70,28 @@ public class Main extends JavaPlugin implements Listener, TabExecutor {
 
             String name = e.getCurrentItem().getItemMeta().getDisplayName().replace("§e", "");
 
-            var obj = MarketplaceGUI.get(name);
+            JsonObject obj = MarketplaceGUI.get(name);
             if (obj == null) return;
 
-            downloadMode(name, obj.getString("url"), p);
+            String url = obj.get("url").getAsString();
+
+            downloadMode(name, url, p);
         }
-
-        // Life GUI
-        if (e.getView().getTitle().equals(LifeGUI.getTitle(p))) {
-            e.setCancelled(true);
-        }
-
-        // Language GUI
-        if (e.getView().getTitle().equals(LanguageGUI.getTitle(p))) {
-            e.setCancelled(true);
-        }
-
-        // Mode GUI
-        if (e.getView().getTitle().equals(ModeGUI.getTitle(p))) {
-            e.setCancelled(true);
-
-            if (e.getCurrentItem() == null) return;
-
-            Material mat = e.getCurrentItem().getType();
-
-            if (mat == Material.REDSTONE_BLOCK) {
-                mode = "HARDCORE";
-            } else if (mat == Material.HEART_OF_THE_SEA) {
-                mode = "LIFESTEAL";
-            } else return;
-
-            getConfig().set("mode", mode);
-            saveConfig();
-
-            p.playSound(p.getLocation(), Sound.UI_BUTTON_CLICK, 1, 1);
-            p.sendMessage("§aMode set to " + mode);
-        }
-    }
-
-    @EventHandler
-    public void onDrag(InventoryDragEvent e) {
-
-        if (!(e.getWhoClicked() instanceof Player p)) return;
-
-        if (e.getView().getTitle().equals(LifeGUI.getTitle(p))) e.setCancelled(true);
-        if (e.getView().getTitle().equals(LanguageGUI.getTitle(p))) e.setCancelled(true);
-        if (e.getView().getTitle().equals(ModeGUI.getTitle(p))) e.setCancelled(true);
     }
 
     // ---------------- COMMANDS ----------------
-
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 
         if (!(sender instanceof Player p)) return true;
 
-        // Mode Builder
         if (cmd.getName().equalsIgnoreCase("mode") && args.length == 2 && args[0].equalsIgnoreCase("create")) {
             ModeBuilderGUI.open(p, args[1]);
             return true;
         }
 
-        // Marketplace
         if (cmd.getName().equalsIgnoreCase("market")) {
             MarketplaceGUI.open(p);
-            return true;
-        }
-
-        // GUIs
-        if (cmd.getName().equalsIgnoreCase("livesgui")) {
-            LifeGUI.open(p);
-            return true;
-        }
-
-        if (cmd.getName().equalsIgnoreCase("langgui")) {
-            LanguageGUI.open(p);
             return true;
         }
 
@@ -283,10 +99,10 @@ public class Main extends JavaPlugin implements Listener, TabExecutor {
     }
 
     // ---------------- DOWNLOAD ----------------
-
     private void downloadMode(String name, String urlStr, Player p) {
 
         Bukkit.getScheduler().runTaskAsynchronously(this, () -> {
+
             try {
                 URL url = new URL(urlStr);
 
@@ -307,7 +123,6 @@ public class Main extends JavaPlugin implements Listener, TabExecutor {
     }
 
     // ---------------- TIMER ----------------
-
     private long getTicksUntilMidnight() {
 
         long now = System.currentTimeMillis();
@@ -320,16 +135,5 @@ public class Main extends JavaPlugin implements Listener, TabExecutor {
         next.add(Calendar.DAY_OF_MONTH, 1);
 
         return (next.getTimeInMillis() - now) / 50;
-    }
-
-    @Override
-    public void onDisable() {
-
-        for (UUID uuid : lives.keySet()) {
-            storage.setLives(uuid, lives.get(uuid));
-            storage.save(uuid);
-        }
-
-        getLogger().info("LifePlugin disabled!");
     }
 }
