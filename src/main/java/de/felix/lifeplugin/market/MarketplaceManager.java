48b87e95f0f12e02d5f6287478a7fb13adb8cb05
Felixwrn <104013013+Felixwrn@users.noreply.github.com>
2026-04-27 16:54:10 +0200

    Refactor load method to use Gson instead of JSONObject

--- src/main/java/de/felix/lifeplugin/market/MarketplaceManager.java
+++ src/main/java/de/felix/lifeplugin/market/MarketplaceManager.java
@@ -1,26 +1,29 @@
 package de.felix.lifeplugin.market;
 
-import org.json.JSONObject;
+import com.google.gson.*;
 import java.net.URL;
 import java.util.*;
 
 public class MarketplaceManager {
 
-    private static final String URL_STRING = "https://raw.githubusercontent.com/YOUR/REPO/main/marketplace.json";
+    private static final String URL_STRING =
+            "https://raw.githubusercontent.com/YOUR/REPO/main/marketplace.json";
 
-    public static Map<String, JSONObject> load(){
+    public static Map<String, JsonObject> load() {
 
-        Map<String, JSONObject> map = new HashMap<>();
+        Map<String, JsonObject> map = new HashMap<>();
 
-        try{
+        try {
             Scanner sc = new Scanner(new URL(URL_STRING).openStream()).useDelimiter("\\A");
-            JSONObject json = new JSONObject(sc.next());
+            String jsonText = sc.next();
 
-            for(String key:json.keySet()){
-                map.put(key,json.getJSONObject(key));
+            JsonObject json = JsonParser.parseString(jsonText).getAsJsonObject();
+
+            for (String key : json.keySet()) {
+                map.put(key, json.getAsJsonObject(key));
             }
 
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
